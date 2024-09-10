26be4f2d681e3d383778a430422ee5915f1334f4
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-09-10 17:32:13 +0200

    Display the ingredients JS + Tailwind (HTML)

--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -1,6 +1,6 @@
 // Card template
 export function getRecipeCardDOM(cardRecipe) {
-    const { image, name, ingredient, quantity, unit, time, description } = cardRecipe;
+    const { image, name, ingredients, time, description } = cardRecipe;
 
     const template = document.getElementById("template-card");
 
@@ -16,12 +16,26 @@ export function getRecipeCardDOM(cardRecipe) {
     const recipeDescription = card.querySelector(".card_content_text_recipe");
     recipeDescription.textContent = `${description}`;
 
-    const textIngredient = card.querySelector(".ingredient");
-    textIngredient.textContent = `${ingredient}`;
-    console.log(textIngredient)
+    const ingredientList = card.querySelector(".card_content_text_ingredient_list");
+    ingredientList.innerHTML = ""; // Réinitialiser le contenu
 
-    const quantityAndUnit = card.querySelector(".quantity_unit");
-    quantityAndUnit.textContent = `${quantity} ${unit}`;
+    // Itérer sur les ingrédients pour les afficher
+    ingredients.forEach(({ ingredient, quantity, unit }) => {
+        const ingredientItem = document.createElement("p"); // Créez un nouvel élément pour chaque ingrédient
+        ingredientItem.classList.add("font-medium");
+        ingredientItem.textContent = `${ingredient}`; // Afficher la quantité et l'unité si elles existent
+        
+        const ingredientQuantityUnity = document.createElement("p");
+        ingredientQuantityUnity.classList.add("text-grey");
+        ingredientQuantityUnity.textContent = `${quantity ? quantity : ""} ${unit ? unit : ""}`
+
+        const ingredientPairing = document.createElement("div");
+
+        ingredientPairing.appendChild(ingredientItem);
+        ingredientPairing.appendChild(ingredientQuantityUnity);
+        ingredientList.appendChild(ingredientPairing);
+        
+    });
 
     const recipeTime = card.querySelector(".time-recipe");
     recipeTime.textContent = `${time}min`;
