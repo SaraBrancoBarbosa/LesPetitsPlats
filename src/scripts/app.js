5430fed65ad7b47ca31004b7a4df5165dfebeade
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-09-20 17:56:53 +0200

    New organisation: filters content and dropdown, new imported functions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,19 +1,9 @@
 //End point to run the application
 import { getRecipeCardDOM } from "./templates/indexCard.js";
 import { getRecipes } from "./api/api.js";
-import { deleteTextButtonHeader, deleteTextButtonFilter } from "./components/searchBars.js";
-import { filterDropDown } from "./components/dropdown.js";
-
-/*********** Calling the json datas and the search bar functions (header + filter) ***********/
-
-// Call the header search bar function to delete the text
-deleteTextButtonHeader();
-
-// Call the filter search bar function to delete the text
-deleteTextButtonFilter();
-
-// To get the json datas
-getRecipes();
+import { enableClearInputText } from "./components/searchBars.js";
+import { initDropDown } from "./components/dropdown.js";
+import { dropdownTag } from "./components/dropdownTag.js";
 
 /*********** Displaying the recipes cards ***********/
 
@@ -29,21 +19,31 @@ async function displayData(recipes) {
         recipiesMainContainer.appendChild(recipeCardDOM);
     });
 
-    /*
-    recipes.forEach((recipe) => {
-        const recipeCardDOM = getRecipeCardDOM(recipe);
-        recipiesMainContainer.appendChild(recipeCardDOM);  
-    });
-    */
-    
-    // Call the filters dropdown function
-    filterDropDown();
 }
 
 /*********** Initiation ***********/
 
 async function init() {
     const { recipes } = await getRecipes();
+    const ingredients = []
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+            if (!ingredients.includes(ingredient.ingredient)) {
+                ingredients.push(ingredient.ingredient)
+            }
+        })
+    })
+
+    /* Build the filters */
+    const filtersContainer = document.querySelector(".filter_container")
+    initDropDown
+    dropdownTag("Ingrédients", "dropdown_ingredients", filtersContainer, ingredients)
+    dropdownTag("Appareils", "dropdown_appliance", filtersContainer, ["hello", "world"])
+    dropdownTag("Ustensiles", "dropdown_utensils", filtersContainer, ["hello", "world"])
+
+    // Call the function to clear the header's input text
+    enableClearInputText(document.querySelector(".inputSearchBarHeader"), document.querySelector(".buttonSearchBarHeader"))
+
     displayData(recipes);
 }
 
