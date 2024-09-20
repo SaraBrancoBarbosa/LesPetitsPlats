f6e57a21676880cd7b7700a2d2cd0f34f8cd121c
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-09-20 18:02:38 +0200

    Code optimisation and click away feature (needs corrections to close the dropdown)

--- src/scripts/components/dropdown.js
+++ src/scripts/components/dropdown.js
@@ -1,69 +1,44 @@
-// To prevent further propagation of the current event
-const stopMediaPropagation = (event) => { 
-    event.stopPropagation();
+import { enableClearInputText } from "./searchBars.js"
+
+const dropdowns = []
+
+export const initDropDown = () => {
+    const dropdownClickAway = document.getElementById("dropdown-clickawaylistener")
+    dropdownClickAway.addEventListener("click", () => {
+        dropdowns.forEach(d => {
+            d.querySelector(".dropdown_filter").style.display = "none"
+            d.querySelector(".icon_filter").classList.remove("icon_open")
+        })
+        dropdownClickAway.style.display = "none"
+    })
 }
 
-export function filterDropDown() {
+export const buildDropdown = (name, id, parent = null) => {
+    const dropdownClickAway = document.getElementById("dropdown-clickawaylistener")
+    const dropdownFragment = document.getElementById("template-component-dropdown").content.cloneNode(true);
+    const dropdown = dropdownFragment.querySelector(".dropdown-container")
+    dropdown.setAttribute("id", id)
 
-    /*********** DOM elements ***********/
+    const button = dropdown.querySelector("#filter_dropdown_button")
+    const buttonName = button.querySelector("#filter_dropdown_button_name")
+    buttonName.textContent = name
 
-    const buttonFilters = document.querySelectorAll(".button_filter");
-    const dropdownFilter = document.querySelector(".dropdown_filter");
-    
-    // Pour "enregistrer" l'icône de base
-    let currentIconFilter = null;
-
-    /*********** Launching and closing filter ***********/
-
-    function openDropdown(pIconFilter) {
-        dropdownFilter.style.display = "flex";
-        pIconFilter.style.transform = "scaleY(1)";
-        pIconFilter.style.transition = "400ms ease";
-        dropdownFilter.setAttribute("opened", true);
-
-        // On mémorise l'icône transformée
-        currentIconFilter = pIconFilter;
-
-        dropdownFilter.addEventListener("click", stopMediaPropagation);
+    button.onclick = () => {
+        dropdowns.forEach(d => {
+            d.querySelector(".dropdown_filter").style.display = "none"
+            d.querySelector(".icon_filter").classList.remove("icon_open")
+        })
+        dropdown.querySelector(".dropdown_filter").style.display = "flex"
+        dropdownClickAway.style.display = "block"
+        const chevron = dropdown.querySelector(".icon_filter")
+        chevron.classList.add("icon_open")
     }
 
-    function closeDropdown(pIconFilter) {
-        dropdownFilter.style.display = "none";
-        pIconFilter.style.transform = "scaleY(-1)";
-        dropdownFilter.setAttribute("opened", false);
+    if (parent) parent.appendChild(dropdown)
 
-        // Remet l'icône à null comme avant l'ouverture (on réinitialise)
-        currentIconFilter = null;
+    dropdowns.push(dropdown)
 
-        dropdownFilter.removeEventListener("click", stopMediaPropagation);
-    }
+    enableClearInputText(dropdown.querySelector(".inputSearchBarFilter"), dropdown.querySelector(".buttonSearchBarFilter"))
 
-    function swapDropdown(pIconFilter) {
-        // Si une icône est déjà ouverte, la fermer avant d'ouvrir la nouvelle
-        if (currentIconFilter && currentIconFilter !== pIconFilter) {
-            closeDropdown(currentIconFilter);
-        }
-        if (dropdownFilter.getAttribute("opened")==="true") {
-            closeDropdown(pIconFilter)
-        } else {
-            openDropdown(pIconFilter)
-        } 
-    }    
-
-    buttonFilters.forEach(button => {
-        button.addEventListener("click", function(event) {
-            
-            // Pour atteindre l'icône du bouton actuellement sélectionné
-            const iconFilter = event.currentTarget.querySelector(".icon_filter");
-            swapDropdown(iconFilter);
-
-            // Pour afficher le dropdown sous le bon bouton
-            const rect = event.target.getBoundingClientRect();
-            dropdownFilter.style.top = `${rect.bottom + window.scrollY - 10}px`;
-            dropdownFilter.style.left = `${rect.left}px`;
-        });
-    });
-
-// Pour les données, faire un switch-case-break ? case "bouton ingrédients sélectionné": "afficher les données" ingrédients: break
-
+    return dropdown
 }
