86eac59da65a997526547bcb76f6d7656ad51421
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-10-07 18:25:45 +0200

    New name (file and function) for better understanding + dispatchEvent()

---
+++ src/scripts/components/searchBar.js
@@ -0,0 +1,26 @@
+export function searchBarInput (input, button) {
+    input?.addEventListener("input", function() {
+
+        // HTML tag injection risks
+        if (input.value.includes("<") || input.value.includes(">")) {
+            input.value = input.value.replace(/<|>/g, "")
+        }
+        
+        // Displays the button only if the field is not empty
+        if (input.value) {
+            button.style.display = "flex"
+        } else {
+            button.style.display = "none"
+        }
+
+    })
+
+    function deleteText() {
+        input.value = ""
+        button.style.display = "none"
+        // Method to invoke an event (input). Bubbles: if true, allows to handle events in a hierarchical way (DOM ancestor/parent)
+        input.dispatchEvent(new Event("input", { bubbles: true }))
+    }
+
+    button?.addEventListener("click", deleteText)
+}
