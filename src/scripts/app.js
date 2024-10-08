87fd233e1c53ded9a6aebe1ec0944fe527561b9c
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-10-08 18:55:02 +0200

    Corrections, comments and optimisation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,48 +1,34 @@
-import { getRecipeCardDOM } from "./templates/indexCard.js"
 import { getRecipes } from "./api/api.js"
-import { searchRecipes } from "./components/searchBars.js"
-import { enableClearInputText } from "./components/searchBars.js"
-import { debounce } from "./components/debounce.js"
-import { initDropDown } from "./components/dropdown.js"
-import { DropdownTag } from "./components/dropdownTag.js"
+import { debounce } from "./lib/debounce.js"
+import { searchRecipes } from "./lib/filters.js"
+import { displayCards } from "./components/displayCards.js"
+import { searchBarInput } from "./components/searchBar.js"
+import { initClickAwayDropdown } from "./components/dropdown.js"
+import { DropdownTag, updateDropdowns } from "./components/dropdownTag.js"
+import { addSelectedTag } from "./components/selectedTagList.js"
 
-// Pour "débouncer" searchRecipes, 350ms de délai
-const debouncedSearchRecipes = debounce(searchRecipes, 500)
+/*********** Initiation ***********/
 
-/*********** Displaying the recipes cards ***********/
-
-// Pour mettre à jour le compteur de recettes (aussi exportée dans la searchBar pour le moment)
-export function updateRecipesCount(count) {
+// To build the recipes count
+const updateRecipesCount = (count) => {
     const recipeCountElement = document.querySelector(".recipes-count")
     recipeCountElement.textContent = `${count} recettes`
 }
 
-// To display the recipes cards
-async function displayData(recipes, page=0, itemsPerPage=-1) {
-    const recipiesMainContainer = document.querySelector(".recipes-main-container")
-    
-    // Si je limite à 10, toutes les recettes ne sont pas prises en compte lors de la recherche...
-    const limitedDisplayRecipes = itemsPerPage > 0 ? recipes.slice(page * itemsPerPage, (page + 1) * itemsPerPage) : recipes
+const filterMapFrEn = new Map([
+    ["ingrédients", "ingredient"],
+    ["appareils", "appliance"],
+    ["ustensiles", "utensil"]
+])
 
-    limitedDisplayRecipes.forEach((recipe) => {
-        const recipeCardDOM = getRecipeCardDOM(recipe)
-        recipiesMainContainer.appendChild(recipeCardDOM)
-    });
-
-    // Pour mettre à jour le compteur avec le nombre de recettes affichées (sinon ça affiche 1500 au début, et ça prend le nombre correct une fois les recherches lancées seulement)
-    updateRecipesCount(recipes.length)
-}
-
-/*********** Initiation ***********/
-
-// Fonction 3 : Main search bar, puis tags, puis affichage
-async function init() {
+window.onload = async () => {
     const { recipes } = await getRecipes()
 
-    // AddEventListener sur la barre de recherche (l'input)
-    document.querySelector(".inputSearchBarHeader").addEventListener("input", debouncedSearchRecipes)
-
     const ingredients = []
+    const appliances = []
+    const utensils = []
+    
+    // For each recipe: to go through the ingredients/appliances/utensils and add them to their list. toLowerCase(): to facilitate comparison and avoid duplicates
     recipes.forEach(recipe => {
         recipe.ingredients.forEach(({ingredient}) => {
             const lowerIngredient = ingredient.toLowerCase()
@@ -51,38 +37,108 @@ async function init() {
         })
     })
 
-    const appliances = []
     recipes.forEach(recipe => {
         const lowerAppliance = recipe.appliance.toLowerCase()
         if (!appliances.find(app => lowerAppliance === app.toLowerCase()))
             appliances.push(recipe.appliance)
     })
 
-    const utensils = []
     recipes.forEach(recipe => {
-        recipe.ustensils.forEach(utensil => {
+        recipe.utensils.forEach(utensil => {
         const lowerUtensils = utensil.toLowerCase()
         if (!utensils.find(ust => lowerUtensils === ust.toLowerCase()))
             utensils.push(utensil)
-        });
-    });
+        })
+    })
 
-    // Build the filters
+    const filterAndDisplayRecipes = () => {
+        // To get the input value and convert it to lowercase to facilitate comparison
+        let filterInput = document.querySelector(".inputSearchBarHeader").value
+        filterInput = filterInput.toLowerCase()
+
+        // To create a list of the selected tags. Language conversion with filterMapFrEn
+        const tagsList = [...document.querySelectorAll(".selected_tag")].map(element => {
+            const tagName = filterMapFrEn.get(element.getAttribute("data-filter").toLowerCase())
+            const tagValue = element.querySelector(".selected_tag_name").textContent.toLowerCase()
+            return { name: tagName, value: tagValue }
+        })
+
+        // To filter the list of recipes (stored in "recipes") based on the value of the input and the list of the selected tags
+        const filtered = searchRecipes(recipes, filterInput, tagsList)
+        const container = document.querySelector(".recipes-main-container")
+        const pagination = document.querySelector(".recipes-pagination")
+
+        // To display the filtered recipes / the no result message
+        if (filtered.length > 0) {
+            displayCards(filtered)
+            pagination.style.display = "flex"
+        } else {
+            container.innerHTML = ""
+            const noResult = document.createElement("div")
+
+            // For the noResult message, we collect the text of the input and the names of the selected tags
+            const selectedTags = tagsList.map(tag => tag.value).join(", ")
+            const searchTerm = filterInput ? `« ${filterInput} »` : ""
+            noResult.textContent = `Aucune recette ne contient ${searchTerm} ${selectedTags ? "et les tags : " + selectedTags : ""}. Vous pouvez chercher « tarte aux pommes », « poisson », etc.`
+            
+            container.appendChild(noResult)
+
+            // When there is no result, the pagination disappears
+            pagination.style.display = "none"
+        }
+
+        // To update the count with the filtered recipes
+        updateRecipesCount(filtered.length)
+
+        updateDropdowns(filtered)
+    }
+
+    // To debounce the filter system
+    const debounceSearchRecipes = debounce(filterAndDisplayRecipes, -1)
+
+    // To build the filters
     const filtersContainer = document.querySelector(".filter_container")
-    initDropDown()
-    const ingredientsFilters = new DropdownTag({name:"Ingrédients", id:"dropdown_ingredients", parent:filtersContainer, tagList:ingredients})
-    const appliancesFilters = new DropdownTag({name:"Appareils", id:"dropdown_appliances", parent:filtersContainer, tagList:appliances})
-    const utensilsFilters = new DropdownTag({name:"Ustensiles", id:"dropdown_utensils", parent:filtersContainer, tagList:utensils})
+    initClickAwayDropdown()
 
-    // Update les résultats
-    ingredientsFilters.update(["Lait de coco"])
+    const dropdowns = [
+        { 
+            name: "Ingrédients", 
+            id: "dropdown_ingredients", 
+            tagList: ingredients 
+        }, { 
+            name: "Appareils", 
+            id: "dropdown_appliances", 
+            tagList: appliances 
+        }, { 
+            name: "Ustensiles", 
+            id: "dropdown_utensils", 
+            tagList: utensils 
+        }
+    ]
+        
+    dropdowns.forEach(({ name, id, tagList }) => {
+        new DropdownTag({
+            name,
+            id,
+            parent: filtersContainer,
+            tagList,
+            onclickTag: (...args) => { addSelectedTag(debounceSearchRecipes, ...args) }
+        })
+    })
 
-    // Call the function to clear the header's input text
-    enableClearInputText(document.querySelector(".inputSearchBarHeader"), document.querySelector(".buttonSearchBarHeader"))
+    /**
+     * onclickTag: this line allows to manage the tag selection logic and to perform a search based on the selected tags
+     * (...args): rest parameters, to accept an indefinite number of arguments as an array
+     * addSelectedTag called with 2 arguments: the debounce and the spread operator (all arguments passed to onclickTag will be passed to addSelectedTag)
+    */
 
-    displayData(recipes)
-}
+    // Header's search bar input: to clear the text (by clicking on the delete btn) and to add the debounce event
+    const inputSearchBarHeader = document.querySelector(".inputSearchBarHeader")
+    searchBarInput(inputSearchBarHeader, document.querySelector(".button_delete_input_text_header"))
+    inputSearchBarHeader.addEventListener("input", debounceSearchRecipes)
+    
+    updateRecipesCount(recipes.length)
 
-window.onload = () => {
-    init()
+    // To display the recipes cards (items per page: 10)
+    displayCards(recipes, 1, 10)
 }
