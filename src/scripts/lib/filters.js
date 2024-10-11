3408970dceb0b9c6cc1d0ffda3bd0be8cb71e2d5
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-10-11 15:34:04 +0200

    Name the 2 versions to compare their performance

--- src/scripts/lib/filters.js
+++ src/scripts/lib/filters.js
@@ -21,8 +21,9 @@ Func 3 =
 */
 
 // Func 3
+// !!!!!!!!!!!!!!!!! Change to filterInputsearchFunctional when splitting branch !!!!!!!!!!!!!!!!!
 export function searchRecipes(list, value, tagsList) {
-    const inNameOrIngredientsOrDescription = filterInputsearch(list, value)
+    const inNameOrIngredientsOrDescription = filterInputsearchNative(list, value)
     const inTags = filterTags(inNameOrIngredientsOrDescription, tagsList)
 
     return inTags
@@ -30,12 +31,12 @@ export function searchRecipes(list, value, tagsList) {
 
 // Func 1
 // Native loops
-export const filterInputsearch = (list, value) => {
+export const filterInputsearchNative = (list, value) => {
     // Below 3 characters, the entire list is returned
     if (value.length < 3) { return [...list] }
 
     const result = []
-    
+
     for (let recipe of list) {
         const name = recipe.name.toLowerCase()
         const ingredients = []
@@ -51,10 +52,9 @@ export const filterInputsearch = (list, value) => {
     return result
 }
 
-/*
 // Func 1
 // Functional programming
-export const filterInputsearch = (list, value) => {
+export const filterInputsearchFunctional = (list, value) => {
     // Below 3 characters, the entire list is returned
     if (value.length < 3) { return [...list] }
 
@@ -67,19 +67,17 @@ export const filterInputsearch = (list, value) => {
         return name.includes(value) || ingredients.some(ingredient => ingredient.includes(value)) || description.includes(value)
     })
 }
-*/
 
 // Func 2
 const filterTags = (list, tagsList) => {
-    
-    // On crée un nouveau tableau avec les recettes qui passent le filtrage
+    // Creates a new array with the filtered recipes
     return list.filter(
         recipe => {
             const ingredients = recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase())
             const appliance = recipe.appliance.toLowerCase()
             const utensils = recipe.utensils.map(utensil => utensil.toLowerCase())
         
-            // every() permet de tester si tous les éléments d'un tableau vérifient une condition donnée par une fonction en argument. Cette méthode renvoie un booléen pour le résultat du test.
+            // every() instances tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value
             return tagsList.every(tag => {
                 if (tag.name === "ingredient") {
                     return ingredients.find(ing => ing === tag.value)
@@ -87,7 +85,7 @@ const filterTags = (list, tagsList) => {
                     return appliance === tag.value
                 } else if (tag.name === "utensil") {
                     return utensils.find(utensil => utensil === tag.value)
-                } // Si le tag ne correspond pas, on retourne false
+                } // If the tag does not match, returns false
                     return false
             })
         }
