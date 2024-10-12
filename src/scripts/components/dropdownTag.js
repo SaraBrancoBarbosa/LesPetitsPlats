674f8150c1b02dcf00e67711fc376e24d318f0d3
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-10-12 12:48:09 +0200

    Filters list when writing in input

--- src/scripts/components/dropdownTag.js
+++ src/scripts/components/dropdownTag.js
@@ -8,6 +8,7 @@ export class DropdownTag {
         const tagTemplate = document.getElementById("template-dropdown-tag-item").content
         const dropdown = buildDropdown(name, id, parent)
         const tagContainer = dropdown.querySelector(".dropdown-tag-container")
+        const inputSearchBarFilter = dropdown.querySelector(".inputSearchBarFilter")
 
         tagList.forEach(tag => {
             const tagElement = tagTemplate.cloneNode(true).querySelector(".dropdown-tag-item-name")
@@ -22,6 +23,12 @@ export class DropdownTag {
             tagContainer.appendChild(tagElement)
         })
 
+        // Quand on écrit dans l'input, ça filtre les listes des tags
+        inputSearchBarFilter.addEventListener("input", () => {
+            const inputValue = inputSearchBarFilter.value.toLowerCase()
+            this.tagsLists(inputValue)
+        })
+
         // On sauvegarde le nom du dropdown
         this.name = name
         // On sauvegarde la liste originale des tags
@@ -31,9 +38,20 @@ export class DropdownTag {
         
     }
 
+    tagsLists(inputValue) {
+        this.tagContainer.querySelectorAll(".dropdown-tag-item-name").forEach(tagElement => {
+            const tag = tagElement.textContent.toLowerCase()
+            // Les tags qui correspondent à ce qu'on écrit dans l'input => flex
+            if (tag.includes(inputValue)) {
+                tagElement.style.display = "flex"
+            } else {
+                tagElement.style.display = "none"
+            }
+        })
+    }
+
     // On prend une nouvelle liste de tags pour update l'affichage dans le dropdown
     update(newTagList) {
-
         // Pour chaque tagElement contenu dans tagContainer
         this.tagContainer.querySelectorAll(".dropdown-tag-item-name").forEach(tagElement => {
             // Si chaque tagElement de newTagList existe dans le dropdown => on le rend visible
@@ -41,16 +59,16 @@ export class DropdownTag {
                 tagElement.style.display = "flex"
             } tagElement.style.display = "none"
         })
-
     }
 }
 
 export const updateDropdowns = (filtered) => {
     /* 
     On cache tous les tags qui ne sont pas dans les recettes affichées
+    Comment faire le lien avec les éléments filtrés par la barre de recherche principale ?
 
     const tagContainer pour chaque dropdowns { "Ingrédients", "Appareils", "Ustensiles" }
-    const tags-dans-recettes-filtrées { "ingredients", "appliances", "utensils" }
+    const tags-dans-recettes-filtrées pour chaque : { "ingredients", "appliances", "utensils" }
     const tagElement = document.querySelectorAll(".dropdown-tag-item-name")
 
     filtered.forEach(recipe => {
