11a216b11af20c34843f57fc01c532839caaafb2
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-10-17 17:36:53 +0200

    Change const name for better understanding

--- src/scripts/lib/debounce.js
+++ src/scripts/lib/debounce.js
@@ -1,20 +1,21 @@
-const map = new Map()
+const runningDebouncedList = new Map()
 
 export const debounce = (callback, delay) => {
     
-    let timeoutId = map.get(callback)
+    let timeoutId = runningDebouncedList.get(callback)
 
     return (...args) => {
 
         if (timeoutId) clearTimeout(timeoutId)
+
             timeoutId = setTimeout(() => {
                 callback(...args)
                 clearTimeout(timeoutId)
                 timeoutId = null
-                map.set(callback, null)
+                runningDebouncedList.set(callback, null)
             }, delay > 0 ? delay : 500 + Math.ceil(Math.random() * 500))
 
-            map.set(callback, timeoutId)
+            runningDebouncedList.set(callback, timeoutId)
 
     }
 
